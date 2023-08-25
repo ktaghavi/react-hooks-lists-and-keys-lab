4fd34664f5b8aeb91fc07a99366726e072572b93
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-08-25 13:00:15 -0600

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techList = technologies.map((eachTech)=>{
+    return (
+      <span>{eachTech}</span>
+    )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techList}
       </div>
     </div>
   );
